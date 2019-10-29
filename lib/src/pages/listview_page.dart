0960160d5d2600ca86d51da3ff959ed60e3efdc3
Ulises Lopez <ulymay@gmail.com>
2019-10-29 14:01:39 -0700

    infinite Scroll

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -8,7 +8,22 @@ class ListaPage extends StatefulWidget {
 
 class _ListaPageState extends State<ListaPage> {
 
-  List<int> _listaNumeros = [1,2,3,4,5];
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _listaNumeros = new List();
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _agregar10();
+
+    _scrollController.addListener(() {
+      if( _scrollController.position.pixels == _scrollController.position.maxScrollExtent ){
+        _agregar10();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +38,7 @@ class _ListaPageState extends State<ListaPage> {
   Widget _crearLista() {
     
     return ListView.builder(
+      controller: _scrollController,
       padding: EdgeInsets.only(top: 0.0),
       itemCount: _listaNumeros.length,
       itemBuilder: ( BuildContext context, int index ) {
@@ -35,6 +51,19 @@ class _ListaPageState extends State<ListaPage> {
         );
       },
     );
+  }
+
+
+
+  void _agregar10() {
+
+    for (var i = 1; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add( _ultimoItem );
+    }
+
+    setState(() {});
+
   }
 
 }
