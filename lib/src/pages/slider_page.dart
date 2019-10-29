ee7cc78a0af418374b50ce693777dab47eae9613
Ulises Lopez <ulymay@gmail.com>
2019-10-29 12:47:36 -0700

    Slider

---
+++ lib/src/pages/slider_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+
+class SliderPage extends StatefulWidget {
+  @override
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+
+  double _valorSlider = 100.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sliders'),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: <Widget>[
+            _crearSlider(),
+            Expanded(
+              child: _crearImagen()
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearSlider() {
+
+    return Slider(
+      activeColor: Colors.indigoAccent,
+      label: 'Tamaño de la imagen',
+      // divisions: 20,
+      value: _valorSlider,
+      min: 10.0,
+      max: 400.0,
+      onChanged: ( valor ){
+
+        setState(() {
+         _valorSlider = valor; 
+        });
+        
+      },
+
+    );
+
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image: NetworkImage('http://pluspng.com/img-png/anime-hd-png-anime-png-pic-png-image-.png'),
+      width: _valorSlider,
+      fit: BoxFit.contain,
+    );
+  }
+
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -6,17 +6,19 @@ import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/slider_page.dart';
 
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
 
       return <String, WidgetBuilder>{
-        '/' :( BuildContext context ) => HomePage(),
-        'alert' :( BuildContext context ) => AlertPage(),
+        '/'      :( BuildContext context ) => HomePage(),
+        'alert'  :( BuildContext context ) => AlertPage(),
         'avatar' :( BuildContext context ) => AvatarPage(),
-        'card' :( BuildContext context ) => CardPage(),
+        'card'   :( BuildContext context ) => CardPage(),
         'animatedContainer' :( BuildContext context ) => AnimatedContainerPage(),
         'inputs' :( BuildContext context ) => InputPage(),
+        'slider' :( BuildContext context ) => SliderPage(), 
       };
 
 }
